1016a5b45893b1d76680ebb47f38094e0591d44e
Varun1o1o <144794316+Varun1o1o@users.noreply.github.com>
2024-08-23 22:52:27 +0530

    Create Day1_Q1.cpp

---
+++ Day1_Q1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string inp1,inp2;
+    cin >> inp1;
+    cin >> inp2;
+    
+    cout << inp1+inp2<<endl;
+    
+
+    return 0;
+}
