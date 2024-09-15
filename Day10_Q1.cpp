a23b546a858b172fb14101d7aeda559247f1b06e
Varun1o1o <144794316+Varun1o1o@users.noreply.github.com>
2024-09-15 20:41:23 +0530

    Create Day10_Q1.cpp

---
+++ Day10_Q1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int findMax(const int arr[], int size) {
+    int max = arr[0];
+    for (int i = 1; i < size; ++i) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+    int size;
+    cout << "Enter the number of elements: ";
+    cin >> size;
+    
+    int* arr = new int[size];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < size; ++i) {
+        cin >> arr[i];
+    }
+    
+    cout << "Maximum value: " << findMax(arr, size) << endl;
+    
+    delete[] arr;
+    return 0;
+}
